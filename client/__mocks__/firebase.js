4288b796774d36835180248b8becfb1ae4ea6d80
Liam-boston <liam.boston@yahoo.com>
2024-07-10 15:39:52 -0400

    Related to GTR-145 - added doc and setDoc functions to the mock config file

--- client/__mocks__/firebase.js
+++ client/__mocks__/firebase.js
@@ -10,3 +10,5 @@ export const getAuth = jest.fn(() => ({
 }));
 export const collection = jest.fn();
 export const getDocs = jest.fn();
+export const setDoc = jest.fn();
+export const doc = jest.fn();
