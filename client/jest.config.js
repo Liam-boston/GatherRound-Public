1365da869341c3561a9791b0dbf1743b7215d5a2
Liam-boston <liam.boston@yahoo.com>
2024-07-10 13:15:09 -0400

    completed firebase emulator config

--- client/jest.config.js
+++ client/jest.config.js
@@ -1,13 +1,18 @@
 module.exports = {
+  // Other configurations...
+  testEnvironment: "jsdom",
   setupFiles: ['./jest.setup.js'],
   transform: {
     "^.+\\.jsx?$": "babel-jest",
   },
-  testEnvironment: "jsdom",
   transformIgnorePatterns: [
     "/node_modules/"
   ],
   moduleNameMapper: {
     "\\.(css|less|scss|sass)$": "jest-transform-stub"
-  }
+  },
+  globals: {
+    'process.env.NODE_ENV': 'test',
+    'process.env.FIRESTORE_EMULATOR_HOST': 'localhost:8080',
+  },
 };
--- client/src/services/firebase.js
+++ client/src/services/firebase.js
@@ -1,8 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getFirestore } from 'firebase/firestore';
+import { getFirestore, connectFirestoreEmulator } from 'firebase/firestore';
 import { getAuth } from "firebase/auth";
-
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -22,6 +21,15 @@ const app = initializeApp(firebaseConfig);
 // Initialize Auth & Firestore
 const db = getFirestore(app);
 const auth = getAuth(app);
+
+// NOTE: Firebase Local Emulator is all set-up and configured for use, I believe you just need to uncomment
+// these lines and it will work. I set it up while testing the Homepage but eventually figured out how to Mock
+// Firebase and pursued that instead. - Liam
+
+// Connect Firestore emulator during testing
+// if (process.env.NODE_ENV === 'test') {
+//   connectFirestoreEmulator(db, process.env.FIRESTORE_EMULATOR_HOST);
+// }
 
 // Export Auth & Firestore
 export { db, auth };
