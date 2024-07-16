48a33c8fbc82668f5bf3c4a128533e6d456e740a
Liam-boston <liam.boston@yahoo.com>
2024-07-16 12:20:23 -0400

    Related to GTR-13 - added data-testids to CreateMeetingModal for ClubDetails testing

--- client/src/components/clubs/CreateMeetingModal.jsx
+++ client/src/components/clubs/CreateMeetingModal.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from "react";
 import "./CreateMeetingModal.css";
 import { collection, addDoc } from "firebase/firestore";
-import { db } from "../../services/firebase"
+import { db } from "../../services/firebase";
 
 const CreateMeetingModal = ({ show, onClose, setMessage, currentUser }) => {
     const [meetingName, setMeetingName] = useState(""); // State to store the club name
     const [meetingDescription, setMeetingDescription] = useState(""); // State to store the club description
 
     const handleSubmit = async (e) => {
-        e.preventDefault(); 
+        e.preventDefault();
 
         // If the user is not logged in, show an error message
         if (!currentUser) {
@@ -23,8 +23,8 @@ const CreateMeetingModal = ({ show, onClose, setMessage, currentUser }) => {
                 description: meetingDescription,
                 creator: [currentUser.uid], // Add the current user to the creator list
                 createdAt: new Date(), // Set the creation date
-                attendees:[], //Initialize empty list of attendees
-                activities:[] //Initialize empty list of activities        
+                attendees: [], // Initialize empty list of attendees
+                activities: [] // Initialize empty list of activities        
             });
 
             // Clear both input fields
@@ -50,11 +50,11 @@ const CreateMeetingModal = ({ show, onClose, setMessage, currentUser }) => {
     }
 
     return (
-        <div className='create-activity-modal__wrapper'>
-            <div className='create-activity-modal__content'>
+        <div className='create-meeting-modal__wrapper' data-testid='create-meeting-modal'>
+            <div className='create-meeting-modal__content'>
                 <h2>Create a New Meeting</h2>
                 <form onSubmit={handleSubmit}>
-                    <div className='create-activity-modal__input-box'>
+                    <div className='create-meeting-modal__input-box'>
                         <input 
                             id='meeting-name' 
                             type='text' 
@@ -64,17 +64,30 @@ const CreateMeetingModal = ({ show, onClose, setMessage, currentUser }) => {
                             required 
                         />
                     </div>
-                    <div className='create-activity-modal__input-box'>
+                    <div className='create-meeting-modal__input-box'>
                         <textarea 
-                            id='activity-description' 
+                            id='meeting-description' 
                             placeholder='Description' 
                             value={meetingDescription} 
                             onChange={(e) => setMeetingDescription(e.target.value)}
                         ></textarea>
                     </div>
-                    <div className='create-activity-modal__buttons'>
-                        <button className='create-activity-modal__cancel' type='button' onClick={onClose}>Cancel</button>
-                        <button className='create-activity-modal__submit' type='submit'>Create Meeting</button>
+                    <div className='create-meeting-modal__buttons'>
+                        <button 
+                            className='create-meeting-modal__cancel' 
+                            type='button' 
+                            onClick={onClose}
+                            data-testid='create-meeting-modal__cancel'
+                        >
+                            Cancel
+                        </button>
+                        <button 
+                            className='create-meeting-modal__submit' 
+                            type='submit'
+                            data-testid='create-meeting-modal__submit'
+                        >
+                            Create Meeting
+                        </button>
                     </div>
                 </form>
             </div>
