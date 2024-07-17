25a9fd2689f4f5a2a42bcae1e702ba486940110c
Liam-boston <liam.boston@yahoo.com>
2024-07-17 13:39:52 -0400

    Related to GTR-13 - added functions necessary to test the club page to mock config

--- client/__mocks__/firebase.js
+++ client/__mocks__/firebase.js
@@ -1,6 +1,9 @@
 // Configuration for mocking Firestore (for testing purposes)
 export const initializeApp = jest.fn();
 export const getFirestore = jest.fn();
+
+// Firebase Auth functions
+export const onAuthStateChanged = jest.fn();
 export const getAuth = jest.fn(() => ({
   onAuthStateChanged: jest.fn((callback) => {
     callback(mockUser); // Involve callback with mockUser
@@ -8,7 +11,14 @@ export const getAuth = jest.fn(() => ({
   }),
   signOut: jest.fn()
 }));
+
+// Firebase Firestore functions
 export const collection = jest.fn();
+export const doc = jest.fn();
+export const addDoc = jest.fn();
 export const getDocs = jest.fn();
 export const setDoc = jest.fn();
-export const doc = jest.fn();
+export const updateDoc = jest.fn();
+export const query = jest.fn();
+export const orderBy = jest.fn();
+export const arrayRemove = jest.fn();
