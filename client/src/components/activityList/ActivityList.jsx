cfd3f1d4c213ac907811e3c7afd66ccdc06c6357
Liam-boston <liam.boston@yahoo.com>
2024-08-06 20:45:28 -0400

    Related to GTR-8 - added Vote button to the ActivityList

--- client/src/components/activityList/ActivityList.jsx
+++ client/src/components/activityList/ActivityList.jsx
@@ -1,145 +1,157 @@
-import "./ActivityList.css"; 
+import "./ActivityList.css";
 import React, { useState, useEffect } from "react";
-import { Navigate, useNavigate, useLocation } from 'react-router-dom';
+import { Navigate, useNavigate, useLocation } from "react-router-dom";
 import { doc, getDoc, collection, getDocs } from "firebase/firestore";
-import { db, auth } from "../../services/firebase"
-import { signOut, getAuth } from 'firebase/auth';
+import { db, auth } from "../../services/firebase";
+import { signOut, getAuth } from "firebase/auth";
 import CreateButton from "../common/CreateButton/CreateButton";
 import CreateActivityModal from "./CreateActivityModal";
 import ProfileButton from "../common/ProfileButton/ProfileButton";
 import UserProfileModal from "../common/UserProfileModal";
 
-
-
 function ActivityList() {
-    const navigate = useNavigate();
-    const [showModal, setShowModal] = useState(false); // State to control modal visibility
-    const [currentUser, setCurrentUser] = useState(null); // State to store the current user
-    const [message, setMessage] = useState(null); // State to hold the success or failure message
-    const [activities, setActivities] = useState([]); // State to hold the list of clubs stored in Firestore
-    const [showUserProfileModal, setShowUserProfileModal] = useState(false); 
-    const [userData, setUserData] = useState(null);
-    const {state} = useLocation();
-    const { meetingID, clubID } = state;
+  const navigate = useNavigate();
+  const [showModal, setShowModal] = useState(false); // State to control modal visibility
+  const [currentUser, setCurrentUser] = useState(null); // State to store the current user
+  const [message, setMessage] = useState(null); // State to hold the success or failure message
+  const [activities, setActivities] = useState([]); // State to hold the list of clubs stored in Firestore
+  const [showUserProfileModal, setShowUserProfileModal] = useState(false);
+  const [userData, setUserData] = useState(null);
+  const { state } = useLocation();
+  const { meetingID, clubID } = state;
 
+  // Fetch the current user from Firebase Auth
+  useEffect(() => {
+    const auth = getAuth();
 
-    // Fetch the current user from Firebase Auth
-    useEffect(() => {
-        const auth = getAuth();
+    const unsubscribe = auth.onAuthStateChanged((user) => {
+      if (user) {
+        setCurrentUser(user);
+      } else {
+        setCurrentUser(null);
+      }
+    });
 
-        const unsubscribe = auth.onAuthStateChanged((user) => {
-            if (user) {
-                setCurrentUser(user);
-            } else {
-                setCurrentUser(null);
-            }
-        });
+    // Clean up the subscription on unmount
+    return () => unsubscribe();
+  }, []);
 
-        // Clean up the subscription on unmount
-        return () => unsubscribe();
-    }, []);
-
-        // Fetch clubs from Firestore
-        useEffect(() => {
-            const fetchActivities = async () => {
-                try {
-                    const querySnapshot = await getDocs(collection(db, "Clubs", clubID, "Meetings", meetingID, "Activities")); // Fetch the Activities collection
-                    const activitiesList = querySnapshot.docs.map(doc => ({
-                        id: doc.id,
-                        ...doc.data()
-                    }));
-                    setActivities(activitiesList); // Update state with fetched activities
-                } catch (error) {
-                    console.error("Error fetching activities: ", error);
-                }
-            };
-    
-            fetchActivities(); // Call fetchActivities when component mounts or currentUser changes
-        }, [currentUser]);
-
-    // Function to handle showing the modal
-    const viewModal = () => {
-        setMessage(null); // Reset the message when the modal is opened
-        setShowModal(true); // Show the modal when Create Club is clicked
-    }
-
-    // Function to handle closing the modal
-    const closeModal = () => {
-        setShowModal(false); // Close the modal
-    }
-
-    // Function to set the success or failure message
-    const handleSetMessage = (newMessage) => {
-        setMessage(newMessage);
-        setTimeout(() => {
-            setMessage(null);
-        }, 3000); // Hide the message after 3 seconds
+  // Fetch clubs from Firestore
+  useEffect(() => {
+    const fetchActivities = async () => {
+      try {
+        const querySnapshot = await getDocs(
+          collection(db, "Clubs", clubID, "Meetings", meetingID, "Activities")
+        ); // Fetch the Activities collection
+        const activitiesList = querySnapshot.docs.map((doc) => ({
+          id: doc.id,
+          ...doc.data(),
+        }));
+        setActivities(activitiesList); // Update state with fetched activities
+      } catch (error) {
+        console.error("Error fetching activities: ", error);
+      }
     };
 
-    const viewUserProfileModal = () => {
-        const docRef = doc(db, 'Users', currentUser.uid);
-        if (!userData) {
-            getDoc(docRef)
-                .then((snapshot) => {
-                    setUserData({
-                        name: snapshot.get('name'),
-                        email: snapshot.get('email')
-                    });
-                })
-                .catch(err => {
-                    console.log(err.message, err)
-                });
-        }
+    fetchActivities(); // Call fetchActivities when component mounts or currentUser changes
+  }, [currentUser]);
 
-        setShowUserProfileModal(true);
+  // Function to handle showing the modal
+  const viewModal = () => {
+    setMessage(null); // Reset the message when the modal is opened
+    setShowModal(true); // Show the modal when Create Club is clicked
+  };
+
+  // Function to handle closing the modal
+  const closeModal = () => {
+    setShowModal(false); // Close the modal
+  };
+
+  // Function to set the success or failure message
+  const handleSetMessage = (newMessage) => {
+    setMessage(newMessage);
+    setTimeout(() => {
+      setMessage(null);
+    }, 3000); // Hide the message after 3 seconds
+  };
+
+  const viewUserProfileModal = () => {
+    const docRef = doc(db, "Users", currentUser.uid);
+    if (!userData) {
+      getDoc(docRef)
+        .then((snapshot) => {
+          setUserData({
+            name: snapshot.get("name"),
+            email: snapshot.get("email"),
+          });
+        })
+        .catch((err) => {
+          console.log(err.message, err);
+        });
     }
 
-    const closeUserProfileModal = () => {
-        setShowUserProfileModal(false);
-    }
+    setShowUserProfileModal(true);
+  };
 
-    const logOut = () => {
-        signOut(auth)
-        return <Navigate to="/" />;
-    }
+  const closeUserProfileModal = () => {
+    setShowUserProfileModal(false);
+  };
 
-    return (
-        <div>
-            <ProfileButton onClick={viewUserProfileModal} />
-            <div className='header'>
-                <h1>{meetingID}</h1>
-                <p>List of activities</p>
-                <button onClick={(e) => navigate(-1)}> Back to Meeting Page </button>
-            </div>
-            <div className='activity-wrapper'>
-                <div className='scrollable-list'>
-                    {/* Scrollable list of club buttons */}
-                    {activities.map((activity, index) => (
-                        <button onClick={(e) => null} key={index} className='club-button'>{activity.name} | Min-Max: {activity.minPlayers}-{activity.maxPlayers} | Description: {activity.description}</button>
-                        // Mapping through tempClubList to create club buttons
-                    ))}
-                </div>
-                {/* Display the success or failure message upon club creation */}
-                {message && (
-                    <div className={`message ${message.type}`}>
-                        {message.text}
-                    </div>
-                )}
-                <div className='create-club'>
-                    {/* Create club button */}
-                    {/* TODO: What's this about? */}
-                    <CreateButton onClick={viewModal} />
-                </div>
-                <div className='create-club-modal'>
-                    {/* Render the modal */}
-                    <CreateActivityModal show={showModal} onClose={closeModal} setMessage={handleSetMessage} currentUser={currentUser} clubID={clubID} />
-                </div>
-                <div className="user-profile-modal">
-                <UserProfileModal show={showUserProfileModal} onClose={closeUserProfileModal} logOut={logOut} userData={userData} />
-                </div>
-            </div>
+  const logOut = () => {
+    signOut(auth);
+    return <Navigate to="/" />;
+  };
+
+  // Function to navigate to the Vote page
+  const handleVoteClick = () => {
+    navigate(`/Homepage/Clubs/${clubID}/${meetingID}/Vote`, {
+      state: { meetingID, clubID },
+    });
+  };  
+
+  return (
+    <div>
+      <ProfileButton onClick={viewUserProfileModal} />
+      <div className="header">
+        <h1>{meetingID}</h1>
+        <p>List of activities</p>
+        <button onClick={() => navigate(-1)}> Back to Meeting Page </button>
+      </div>
+      <div className="activity-wrapper">
+        <div className="scrollable-list">
+          {/* Scrollable list of club buttons */}
+          {activities.map((activity, index) => (
+            <button
+              onClick={(e) => null}
+              key={index}
+              className="club-button"
+            >
+              {activity.name} | Min-Max: {activity.minPlayers}-
+              {activity.maxPlayers} | Description: {activity.description}
+            </button>
+            // Mapping through tempClubList to create club buttons
+          ))}
         </div>
-    );
+        {/* Display the success or failure message upon club creation */}
+        {message && (
+          <div className={`message ${message.type}`}>{message.text}</div>
+        )}
+        <div className="vote-button-wrapper">
+          <button className="vote-button" onClick={handleVoteClick}>
+            Vote
+          </button>
+        </div>
+        <div className="user-profile-modal">
+          <UserProfileModal
+            show={showUserProfileModal}
+            onClose={closeUserProfileModal}
+            logOut={logOut}
+            userData={userData}
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ActivityList;
