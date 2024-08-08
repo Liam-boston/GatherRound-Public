0baa4f421201258bc7bedbdff40b5b50df86b8f8
AlexandrosSfikas <alex.sfikas@gmail.com>
2024-08-08 17:34:24 +0300

    Leave Club button now works, only admins can create new meetings

--- client/src/components/clubs/ClubDetails.jsx
+++ client/src/components/clubs/ClubDetails.jsx
@@ -6,18 +6,18 @@ import { signOut, getAuth } from 'firebase/auth';
 import ProfileButton from "../common/ProfileButton/ProfileButton";
 import CreateMeetingModal from "./CreateMeetingModal";
 import CreateButton from "../common/CreateButton/CreateButton"; 
-import { doc, getDoc, collection, getDocs, orderBy, query } from 'firebase/firestore';
+import { doc, getDoc, collection, getDocs, orderBy, query, deleteDoc, arrayRemove, updateDoc } from 'firebase/firestore';
 import { auth, db } from "../../services/firebase";
 import UserProfileModal from "../common/UserProfileModal";
 
 function ClubDetails() {
-
     const navigate = useNavigate();
     const { id } = useParams();
     const [currentUser, setCurrentUser] = useState(null); // State to store the current user
     const [showModal, setShowModal] = useState(false); // State to control modal visibility
     const [message, setMessage] = useState(null); // State to hold the success or failure message
     const [meetings, setMeetings] = useState([]); // State to hold the list of meetings stored in Firestore
+    const [admin, setIsAdmin] = useState(false); // State to hold the admin status of the user
     const [showUserProfileModal, setShowUserProfileModal] = useState(false); 
     const docRef1 = collection(db, "Clubs", id, "Meetings");
     const [userData, setUserData] = useState(null);
@@ -28,6 +28,7 @@ function ClubDetails() {
         const unsubscribe = auth.onAuthStateChanged((user) => {
             if (user) {
                 setCurrentUser(user);
+                checkAdminStatus(user.uid);
             } else {
                 setCurrentUser(null);
             }
@@ -37,6 +38,7 @@ function ClubDetails() {
         return () => unsubscribe();
     }, []);
 
+ 
     // Fetch meetings from Firestore
     useEffect(() => {
         const fetchMeetings = async () => {
@@ -57,6 +59,21 @@ function ClubDetails() {
     }, [currentUser, showModal]);
 
 
+    // Check if the current user is an admin
+    const checkAdminStatus = async (userId) => {
+        try {
+            const memberDocRef = doc(db, "Clubs", id, "Members", userId);
+            const memberDoc = await getDoc(memberDocRef);
+            if (memberDoc.exists()) {
+                setIsAdmin(memberDoc.data().admin);
+                console.log(admin);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+     //Function to handle viewing the profile modal
     const viewUserProfileModal = () => {
         const docRef = doc(db, 'Users', currentUser.uid);
         if (!userData) {
@@ -75,10 +92,12 @@ function ClubDetails() {
         setShowUserProfileModal(true);
     }
 
+    //Function to handle closing the profile modal
     const closeUserProfileModal = () => {
         setShowUserProfileModal(false);
     }
 
+    //Logout function
     const logOut = () => {
         signOut(auth)
         return <Navigate to="/" />;
@@ -86,8 +105,10 @@ function ClubDetails() {
 
     // Function to handle showing the modal
     const viewModal = () => {
-        setMessage(null); // Reset the message when the modal is opened
-        setShowModal(true); // Show the modal when Create Club is clicked
+        if(admin == true ){
+            setMessage(null); // Reset the message when the modal is opened
+            setShowModal(true); // Show the modal when Create Club is clicked
+        }
     }
 
     // Function to handle closing the modal
@@ -103,6 +124,16 @@ function ClubDetails() {
         }, 3000); // Hide the message after 3 seconds
     };
 
+    // Function to handle leaving the club
+    const handleLeaveClub = async () => {
+        const docRef2 = doc(db, 'Clubs', id, 'Members', currentUser.uid);
+        const docRef3 = doc(db, 'Users', currentUser.uid);
+        await deleteDoc(docRef2);
+        await updateDoc(docRef3, {clubNames: arrayRemove(id)});
+        navigate("/Homepage");
+    }
+
+
     return (
             <div>
                  <ProfileButton onClick={viewUserProfileModal} />
@@ -116,7 +147,7 @@ function ClubDetails() {
                                 {/* List of options*/} 
                                     <button type="button" onClick={(e) => navigate("MemberList")}  className='options'>List of Members</button>  
                                     <button type="button" onClick={(e) => navigate("/Homepage")}  className='options'>Return Home</button>  
-                                    <button type="button" onClick={(e) => null}  className='options'>Leave Club</button>  
+                                    <button type="button" onClick={handleLeaveClub}  className='options'>Leave Club</button>  
                             </div>
                     </div>
                         <div className='activity-wrapper'>
@@ -135,7 +166,7 @@ function ClubDetails() {
                              )}
                             <div className='create-club'>
                                 {/* Create meeting button */}
-                                <CreateButton onClick={viewModal} />
+                                <CreateButton onClick={viewModal}/>
                             </div>
                             <div className='create-club-modal'>
                                 {/* Render the modal */}
